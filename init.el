88a8095e5fc5091f493b6a329025576e8a4a47b4
Walter de Jong <walter@heiho.net>
2026-01-23 23:51:30 +0100

    my custom emacs config

---
+++ init.el
@@ -0,0 +1,138 @@
+;;
+;;  ~/.emacs.d/init.el
+;;
+
+(setq inhibit-splash-screen t)
+
+(add-to-list 'load-path "/home/walter/.emacs.d/better-defaults")
+(require 'better-defaults)
+
+(set-frame-font "Source Code Pro Medium 11" nil t)
+
+(setq-default cursor-type 'box)
+(blink-cursor-mode -1)
+
+(setq-default tab-width 4)
+
+(tool-bar-mode -1)
+
+(electric-pair-mode 1)
+
+(setq gc-cons-threshold 100000000) ; 100 mb
+(setq read-process-output-max (* 1024 1024)) ; 1mb
+
+(add-to-list 'custom-theme-load-path "/home/walter/.emacs.d/themes/")
+(load-theme 'zenburn t)
+
+(set-cursor-color "#00ee00")
+
+(setq-default truncate-lines t)
+(if (window-system) (set-frame-size (selected-frame) 213 44))
+(split-window-right)
+
+;; (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(global-display-line-numbers-mode t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "#000000" :foreground "#00cc00")))))
+
+;; save without query
+(add-hook 'find-file-hook (lambda () (setq buffer-save-without-query t)))
+
+(cua-mode t)
+
+(global-unset-key (kbd "C-k"))
+(global-unset-key (kbd "C-b"))
+(global-unset-key (kbd "C-w"))
+
+(global-set-key (kbd "C-x C-b") 'switch-to-buffer)
+
+(global-set-key (kbd "C-o") 'find-file)
+;; C-s clashes with Search, and I can't easily redefine <C-s C-s> for repeated search
+;; (global-set-key (kbd "C-s") 'save-some-buffers)
+(global-set-key (kbd "C-y") 'kill-whole-line)
+;; (global-set-key (kbd "C-f") 'isearch-forward)
+(global-set-key (kbd "C-j") 'goto-line)
+ 
+(global-set-key (kbd "C-z") 'undo)
+
+(defvar-keymap walter-wordstar-prefix-map
+  :doc "WordStar-like map. It's my custom map, so it's not exact."
+  "a" #'mark-whole-buffer
+  "b" #'set-mark-command
+  "c" #'kill-ring-save
+  "y" #'kill-region
+  "DEL" #'kill-region
+  "v" #'yank
+  "f" #'isearch-forward
+  "g" #'replace-string
+  "r" #'replace-string
+  "i" #'string-insert-rectangle
+  "/" #'comment-line
+  "s" #'save-some-buffers
+  "C-s" #'save-some-buffers
+  "w" #'write-file
+  "x" #'save-buffers-kill-emacs
+  "q" #'kill-emacs
+)
+
+(keymap-set global-map "C-k" walter-wordstar-prefix-map)
+
+(defvar-keymap walter-buffer-prefix-map
+  :doc "buffer commands."
+  "b" #'switch-to-buffer
+  "C-b" #'switch-to-buffer
+  "{" #'previous-buffer
+  "}" #'next-buffer
+  "<" #'previous-buffer
+  ">" #'next-buffer
+  "<left>" #'previous-buffer
+  "<right>" #'next-buffer
+  "n" #'rename-buffer
+  "s" #'save-buffer
+  "w" #'write-file
+  "d" #'kill-buffer
+  "q" #'kill-buffer
+)
+
+(keymap-set global-map "C-b" walter-buffer-prefix-map)
+
+(defvar-keymap walter-window-prefix-map
+  :doc "window commands."
+  "w" #'other-window
+  "C-w" #'other-window
+  "o" #'other-window
+  "<" #'other-window
+  ">" #'other-window
+  "<left>" #'other-window
+  "<right>" #'other-window
+  "d" #'delete-window
+  "|" #'split-window-right
+  "-" #'split-window-below
+)
+
+(keymap-set global-map "C-w" walter-window-prefix-map)
+
+;; (define-minor-mode walter-mode
+;;   "Get your foos in the right places."
+;;   :lighter " WJ"
+;;   :keymap (let ((map (make-sparse-keymap)))
+;;             (define-key map (kbd "C-w w") 'other-window)
+;;             map))
+;; 
+;; ;;;###autoload
+;; (add-hook 'text-mode-hook 'walter-mode)
+;; 
+;; (provide 'walter-mode)
+
+;; EOB
